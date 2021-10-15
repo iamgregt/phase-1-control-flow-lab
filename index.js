d21dfd3c34f8701d47d4d12885b4cc1acc26687e
Gregory <taylor.gregory2@gmail.com>
2021-10-14 18:23:21 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue < 400){
+    result = "This one is on me!";
+  } else if(someValue > 2000 && someValue <= 2500 ){
+    result = "I will gladly take your thirty bucks.";
+  } else if(someValue > 2500 ){
+    result = 'No can do.';
+  }
+
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const reply = (city === "NYC" ? "Ok, sounds good." : "No go.");
+  return reply
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let thanks
+  switch (tip){
+    case 'generous':
+      thanks = "Thank you so much.";
+      break;
+    case 'not as generous':
+      thanks = "Thank you.";
+      break;
+    default:
+      thanks = "Bye.";
+      break;
+  }
+  return thanks
 }
